b8d9784d4d6c96700f0c476d4dd099d5bd52e71a
Christopher Mackay <cmackay732@gmail.com>
2020-04-23 21:12:55 -0400

    added remove-element

---
+++ remove-element/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace remove_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0,1,2,2,3,0,4,2 };
+            int val = 2;
+            Console.WriteLine(RemoveElement(nums, val));
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            List<int> l = new List<int>();
+
+            foreach (int n in nums)
+                l.Add(n);
+
+            l.RemoveAll(x => x == val);
+
+            return l.Count;
+        }
+    }
+}
