a5c58a10487a39ce0c302331a17b366b809b4962
Christopher Mackay <cmackay732@gmail.com>
2020-05-02 14:51:15 -0400

    added sqrtx

---
+++ sqrtx/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sqrtx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MySqrt(8).ToString());
+        }
+
+        public static int MySqrt(int x)
+        {
+            return Convert.ToInt32(Math.Truncate(Math.Sqrt(x)));
+        }
+    }
+}
