00c17cd46fc37dc213df4775def536519277a712
Christopher Mackay <cmackay732@gmail.com>
2020-05-02 12:37:53 -0400

    added length-of-last-word

---
+++ length-of-last-word/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace length_of_last_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "day";
+            int i = LengthOfLastWord(s);
+            Console.WriteLine(i);
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+            s = s.TrimEnd();
+            if (s.Length == 0) return 0;
+            if (s == " ") return 0;
+            if (s.Length == 1) return 1;
+            
+            int l = 0;
+
+            if (s.LastIndexOf(" ") != -1)
+                l = s.Length - s.Substring(0, s.LastIndexOf(" ")).Length - 1;
+            else
+                l = s.Length;
+
+            return l;
+        }
+    }
+}
