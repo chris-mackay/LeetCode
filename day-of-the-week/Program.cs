53e6bab3e635d747b8b21433abdc98b0378d697a
Christopher Mackay <cmackay732@gmail.com>
2020-05-02 15:45:54 -0400

    added day-of-the-week

---
+++ day-of-the-week/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace day_of_the_week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayOftheWeek(31, 8, 2019));
+        }
+
+        public static string DayOftheWeek(int day, int month, int year)
+        {
+            DateTime d = new DateTime(year, month, day);
+            return d.DayOfWeek.ToString();
+        }
+    }
+}
