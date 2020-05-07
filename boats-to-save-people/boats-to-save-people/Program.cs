187de0c956310bd96acb8fa818b16d55d3f07615
Christopher Mackay <cmackay732@gmail.com>
2020-05-07 19:56:50 -0400

    added problem

---
+++ boats-to-save-people/boats-to-save-people/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boats_to_save_people
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] people = { 3, 5, 3, 4 };
+            int limit = 5;
+
+
+
+
+        }
+    }
+}
