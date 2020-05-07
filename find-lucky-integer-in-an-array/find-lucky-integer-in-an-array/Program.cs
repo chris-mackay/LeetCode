bb04550dc9867d57c00cb037860443a982489d37
Christopher Mackay <cmackay732@gmail.com>
2020-05-06 20:30:58 -0400

    added find-lucky-integer-in-an-array

---
+++ find-lucky-integer-in-an-array/find-lucky-integer-in-an-array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace findluckyintegerinanarray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 2, 3, 3, 3 };
+            Dictionary<int, int> d = new Dictionary<int, int>();
+
+            foreach (int n in a)
+            {
+                int c = (from num in a where num == n select num).Count();
+                if (!d.Keys.Contains(n))
+                    d.Add(n, c);
+            }
+
+            foreach (KeyValuePair<int, int> entry in d)
+                if (entry.Key == entry.Value)
+                    Console.WriteLine("Number: {0}, and Count: {1}", entry.Key, entry.Value);
+
+        }
+    }
+}
---
+++ k-concatenation-maximum-sum/k-concatenation-maximum-sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace kconcatenationmaximumsum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, -2, 1 };
+            int k = 5;
+            Console.WriteLine(KConcatenationMaxSum(a,k).ToString());
+            Console.ReadLine();
+        }
+
+        public static int KConcatenationMaxSum(int[] arr, int k)
+        {
+            if (arr.Length == 0) return 0;
+            int r = 0;
+
+            for (int i = 0; i <= k; i++)
+                foreach (int s in arr)
+                    r += Convert.ToInt32(s.ToString());
+           
+            return r;
+        }
+
+    }
+}
