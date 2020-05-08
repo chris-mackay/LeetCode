c2434c94087435d15000269628f8f9c7659a2ee1
Christopher Mackay <cmackay732@gmail.com>
2020-05-07 21:10:02 -0400

    not solved yet

--- boats-to-save-people/boats-to-save-people/Program.cs
+++ boats-to-save-people/boats-to-save-people/Program.cs
@@ -10,12 +10,31 @@ namespace boats_to_save_people
     {
         static void Main(string[] args)
         {
-            int[] people = { 3, 5, 3, 4 };
-            int limit = 5;
+            int[] people = { 3,2,2,1 };
+            int limit = 3;
 
+            Console.WriteLine(NumRescueBoats(people, limit).ToString());
+            Console.Read();
 
+        }
 
+        public static int NumRescueBoats(int[] people, int limit)
+        {
+            int weightInBoat = people[0];
+            int boats = 1;
 
+            for (int i = 1; i <= people.Length - 1; i++)
+            {
+                weightInBoat += people[i];
+                
+                if (weightInBoat >= limit)
+                {
+                    boats++;
+                    weightInBoat = 0;
+                }
+            }
+
+            return boats;
         }
     }
 }
