8e3793cd3ad7037468f6af542b6847f4c1b791eb
Christopher Mackay <cmackay732@gmail.com>
2020-05-14 18:43:08 -0400

    added and solved reverse-string

---
+++ reverse-string/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace reversestring
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = "hello";
+            char[] a = s.ToCharArray();
+            ReverseString(a);
+        }
+
+        public static void ReverseString(char[] s)
+        {
+            Array.Reverse(s);
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in s) sb.Append(c);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
