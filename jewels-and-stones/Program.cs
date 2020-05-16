b4764b7a1c59feecc3ee36e907265d63d491ebe1
Christopher Mackay <cmackay732@gmail.com>
2020-05-15 20:05:35 -0400

    added and solved jewels-and-stones

---
+++ jewels-and-stones/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace jewelsandstones
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string j = "aA";
+            string s = "aAAbbbb";
+            Console.WriteLine(NumJewelsInStones(j, s).ToString());
+        }
+
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int n = 0;
+            char[] a = J.ToCharArray();
+            foreach (char c in a)
+                foreach (char ci in S)
+                if (c.ToString() == ci.ToString()) n++;
+
+            return n;
+        }
+    }
+}
