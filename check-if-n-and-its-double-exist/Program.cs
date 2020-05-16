048115b98ba149b893f93ab5a0605b07af3ea48c
Christopher Mackay <cmackay732@gmail.com>
2020-05-15 20:25:55 -0400

    added and solved check-if-n-and-its-double-exist

---
+++ check-if-n-and-its-double-exist/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace checkifnanditsdoubleexist
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = { 10, 2, 5, 3 };
+            Console.WriteLine(CheckIfExist(a));
+        }
+
+        public static bool CheckIfExist(int[] arr)
+        {
+            for (int i = 0; i <= arr.Length; i++)
+                for (int j = 0; j <= arr.Length; j++)
+                {
+                    if (i >= 0)
+                        if (i != j)
+                            if (arr[i] == 2 * arr[j])
+                                return true;
+                }
+            return false;
+        }
+
+    }
+}
