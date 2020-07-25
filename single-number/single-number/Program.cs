4449fe18ffb2b2ccab9b627109ccbc08072b8467
Christopher Mackay <cmackay732@gmail.com>
2020-07-24 21:08:16 -0400

    added divide-two-integers (not solved) added single-number (solved)

---
+++ single-number/single-number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2, 1 };
+            Console.WriteLine(SingleNumber(nums));
+            Console.ReadLine();
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            return nums.GroupBy(i => i).Where(g => g.Count() == 1).Select(g => g.First()).First();
+        }
+    }
+}
