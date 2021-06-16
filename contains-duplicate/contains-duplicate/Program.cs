be1a5291810fb590e7334227c1f92db6113a3409
Christopher Mackay <cmackay732@gmail.com>
2021-06-15 21:06:19 -0400

    Added new problems

---
+++ contains-duplicate/contains-duplicate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contains_duplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 1 };
+            var query = nums.GroupBy(x => x).Where(g => g.Count() > 1).Select(y => y.Key).ToList();
+            bool flag = false;
+
+            if (query.Count > 0) flag = true; else flag = false;
+
+            Console.WriteLine(flag);
+            Console.ReadLine();
+        }
+    }
+}
