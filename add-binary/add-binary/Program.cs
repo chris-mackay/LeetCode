0507c6819b59325bf36f616ab1ac1a8d06c20c86
Christopher Mackay <cmackay732@gmail.com>
2021-10-16 17:19:56 -0400

    solves for small numbers.

--- add-binary/add-binary/Program.cs
+++ add-binary/add-binary/Program.cs
@@ -10,18 +10,57 @@ namespace add_binary
     {
         static void Main(string[] args)
         {
-            string a = "1010";
-            string b = "1011";
+            string a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+            string b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
 
             Console.WriteLine(AddBinary(a, b));
         }
 
         public static string AddBinary(string a, string b)
         {
-            int ai = Convert.ToInt32(a, 2);
-            int bi = Convert.ToInt32(b, 2);
+            bool aIsValid = false;
+            bool bIsValid = false;
+            int bin = 0;
 
-            int bin = ai + bi;
+            if (1 <= a.Length && b.Length <= Math.Pow(10, 4))
+            {
+                foreach (char c in a)
+                {
+                    int i = Convert.ToInt32(c.ToString());
+                    if(i < 0 || i > 1)
+                    {
+                        aIsValid = false;
+                        break;
+                    }
+                    else
+                    {
+                        aIsValid = true;
+                    }
+                }
+
+                foreach (char  c in b)
+                {
+                    int i = Convert.ToInt32(c.ToString());
+                    if (i < 0 || i > 1)
+                    {
+                        bIsValid = false;
+                        break;
+                    }
+                    else
+                    {
+                        bIsValid = true;
+                    }
+                }
+
+                if (aIsValid && bIsValid)
+                {
+                    int ai = Convert.ToInt32(a.ToString(), 2);
+                    int bi = Convert.ToInt32(b.ToString(), 2);
+
+                    bin = ai + bi;
+                    
+                }
+            }
             return Convert.ToString(bin, 2);
         }
     }
